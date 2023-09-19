88f423599564232b57d57a3df6822601b1fd58e1
SLINIcraftet204 <45763476+SLINIcraftet204@users.noreply.github.com>
2023-09-19 12:48:26 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TTT-Games'
+copyright = '2023, SLINIcraftet204 and Haeherfeder'
+author = 'SLINIcraftet204'
 
 release = '0.1'
 version = '0.1.0'
